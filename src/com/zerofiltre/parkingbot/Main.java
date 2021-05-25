b783b715edae1f0ce7488eb32b4da87c85e333c7
philippe SIMO <philippe.guemkamsimo@gmail.com>
2021-05-25 22:13:31 +0200

    Display surface

--- src/com/zerofiltre/parkingbot/Main.java
+++ src/com/zerofiltre/parkingbot/Main.java
@@ -2,7 +2,12 @@ package com.zerofiltre.parkingbot;
 
 public class Main {
 
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+       int length = 180;
+       int width = 120;
+       int surface = length * width;
+       System.out.println("La surface au sol de la voiture est de: "+ surface);
+
     }
 }
