353f05e7dc46a941ce96b058af1d165c8a17d1c5
philippe SIMO <philippe.guemkamsimo@gmail.com>
2021-05-31 22:28:21 +0200

    display Surface enhanced

--- src/com/zerofiltre/parkingbot/Main.java
+++ src/com/zerofiltre/parkingbot/Main.java
@@ -7,7 +7,13 @@ public class Main {
        int length = 180;
        int width = 120;
        int surface = length * width;
-       System.out.println("La surface au sol de la voiture est de: "+ surface);
+       int perimeter = (length + width) * 2;
+       int difference = length - width;
+       float ratio = (float) width / length;
+       System.out.println("La surface au sol de la voiture est de: " + surface);
+       System.out.println("Le périmète de la voiture est de: " + perimeter);
+       System.out.println("La différence entre la longueur et la largeur est de  " + difference);
+       System.out.println("Le ratio entre la longueur et la largeur est de  " + ratio);
 
     }
 }
