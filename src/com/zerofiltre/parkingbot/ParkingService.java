934e64930f2e2688116a5823d3a783f69bbd5678
philippe SIMO <philippe.guemkamsimo@gmail.com>
2021-06-02 22:29:17 +0200

    Variable scope OK

---
+++ src/com/zerofiltre/parkingbot/ParkingService.java
@@ -0,0 +1,9 @@
+package com.zerofiltre.parkingbot;
+
+public class ParkingService {
+
+  public String parking = "Parking et surveillance de véhicules";
+  public String washing = "Nettoyage de véhicules";
+
+
+}
