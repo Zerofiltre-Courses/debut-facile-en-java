a8b56279aff5402fa717bcaba1ca6044ef2d7c1c
philippe SIMO <philippe.guemkamsimo@gmail.com>
2021-06-05 10:16:59 +0200

    p1c5

--- src/com/zerofiltre/parkingbot/ParkingBot.java
+++ src/com/zerofiltre/parkingbot/ParkingBot.java
@@ -15,36 +15,13 @@ public class ParkingBot {
    * @param args : Tableau de données entrées lors du lancement de l'application
    */
   public static void main(String[] args) {
-//    for (int i = 0; i < customers.length; i++) {
-//      sayHello(customers[i]);
-//    }
-//
-//    for (String customer : customers) {
-//      sayHello(customer);
-//    }
-
-    System.out.println("Nous avons " + parkingSize + " place(s) restante(s)");
-    int customerNumber = 0;
-
-    while (parkingSize > 0) {
-      parkingSize--;
-
-      if ("Ali".equals(customers[customerNumber])) {
-        customerNumber++;
-        continue;
-      }
-      if ("Booba".equals(customers[customerNumber])) {
-        break;
-      }
-      sayHello(customers[customerNumber]);
-      customerNumber++;
-    }
 
   }
 
 
   /**
-   * Cette méthode permet de dire Hello
+   *
+   * @param customer Le client à qui dire Hello
    */
   private static void sayHello(String customer) {
     String welcomeSentence = "Hello " + customer;
