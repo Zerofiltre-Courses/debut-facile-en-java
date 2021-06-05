68085f98a6ebd56a78820f2ce8fa2c9ff0e70852
philippe SIMO <philippe.guemkamsimo@gmail.com>
2021-06-05 10:13:45 +0200

    p1c5OK

--- src/com/zerofiltre/parkingbot/ParkingBot.java
+++ src/com/zerofiltre/parkingbot/ParkingBot.java
@@ -1,35 +1,54 @@
 package com.zerofiltre.parkingbot;
 
-import com.zerofiltre.parkingbot.service.ParkingService;
+import java.security.SecureRandom;
 
 public class ParkingBot {
 
-  static String completion = "des équipes parking Zerofiltre!";
-  static ParkingService parkingService = new ParkingService();
+  static String[] customers =
+      {"Leye", "Ali", "Réné", "Booba", "Léa", "Hamza", "Philippe", "Zineb", "Jules", "Mukul", "Will"};
+
+  static int parkingSize = new SecureRandom().nextInt(10);
 
   /**
    * Ceci est la méthode Main
+   *
    * @param args : Tableau de données entrées lors du lancement de l'application
    */
   public static void main(String[] args) {
-    sayHello();
-    sayBye();
+//    for (int i = 0; i < customers.length; i++) {
+//      sayHello(customers[i]);
+//    }
+//
+//    for (String customer : customers) {
+//      sayHello(customer);
+//    }
+
+    System.out.println("Nous avons " + parkingSize + " place(s) restante(s)");
+    int customerNumber = 0;
+
+    while (parkingSize > 0) {
+      parkingSize--;
+
+      if ("Ali".equals(customers[customerNumber])) {
+        customerNumber++;
+        continue;
+      }
+      if ("Booba".equals(customers[customerNumber])) {
+        break;
+      }
+      sayHello(customers[customerNumber]);
+      customerNumber++;
+    }
+
   }
 
 
   /**
    * Cette méthode permet de dire Hello
    */
-  private static void sayHello() {
-    String welcomeSentence = "Hello, recevez la bienvenue " + completion;
+  private static void sayHello(String customer) {
+    String welcomeSentence = "Hello " + customer;
     System.out.println(welcomeSentence);
-    String parkService = parkingService.parking;
-    String washingService = parkingService.washing;
-
-    System.out.println("Nous proposons les services suivants: "+parkService+" et "+washingService);
   }
 
-  private static void sayBye(){
-    System.out.println("Aurevoir et à bientôt "+ completion);
-  }
 }
--- src/com/zerofiltre/parkingbot/service/ParkingService.java
+++
@@ -1,9 +0,0 @@
-package com.zerofiltre.parkingbot.service;
-
-public class ParkingService {
-
-  public String parking = "Parking et surveillance de véhicules";
-  public String washing = "Nettoyage de véhicules";
-
-
-}
