ca5022bf6acfb3f2a36538b95b060949ff0e5553
philippe SIMO <philippe.guemkamsimo@gmail.com>
2021-06-15 08:43:44 +0200

    p1c9OK

--- src/com/zerofiltre/parkingbot/ParkingBot.java
+++ src/com/zerofiltre/parkingbot/ParkingBot.java
@@ -5,6 +5,10 @@ import com.zerofiltre.parkingbot.model.Car;
 import com.zerofiltre.parkingbot.model.Ticket;
 import com.zerofiltre.parkingbot.model.Vehicle;
 import com.zerofiltre.parkingbot.service.ParkingService;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class ParkingBot {
 
@@ -20,24 +24,50 @@ public class ParkingBot {
   }
 
   private static void processVehicles() {
+    Set<Ticket> tickets = new HashSet<>();
+
     Vehicle vehicle = new Vehicle();
     vehicle.setRegistrationNumber("LS-324-PM");
     Ticket vehicleTicket = parkingService.processIncomingVehicle(vehicle);
     System.out.println(vehicleTicket);
+    tickets.add(vehicleTicket);
 
     Vehicle bicycle = new Bicycle();
     bicycle.setRegistrationNumber("PM-254-OP");
     Ticket bicycleTicket = parkingService.processIncomingVehicle(bicycle);
     System.out.println(bicycleTicket);
+    tickets.add(bicycleTicket);
 
     Vehicle car = new Car();
     bicycle.setRegistrationNumber("BX-256-QX");
     Ticket carTicket = parkingService.processIncomingVehicle(car);
     System.out.println(carTicket);
+    tickets.add(carTicket);
 
-    System.out.println(parkingService.processExitingVehicle(vehicleTicket));
-    System.out.println(parkingService.processExitingVehicle(bicycleTicket));
-    System.out.println(parkingService.processExitingVehicle(carTicket));
+  /*  Ticket[] tickets = new Ticket[3];
+    tickets[0] = vehicleTicket;
+    tickets[1] = bicycleTicket;
+    tickets[2] = carTicket;*/
+
+    System.out.println("Traitement de sorties en lot");
+   /* for (int i = 0; i < tickets.size(); i++) {
+      System.out.println(parkingService.processExitingVehicle(tickets.get(i)));
+    }*/
+    Map<Integer, Ticket> exitOrder = new HashMap<>();
+    int position = 0;
+
+    for (Ticket ticket : tickets) {
+      exitOrder.put(position, ticket);
+      position++;
+      System.out.println(parkingService.processExitingVehicle(ticket));
+    }
+
+    Set<Integer> keySet = exitOrder.keySet();
+    for (int key : keySet) {
+      System.out.println("Position dans la liste: " + key + " : " + exitOrder.get(key).getVehicle().getCategory());
+    }
+
   }
+
 
 }
